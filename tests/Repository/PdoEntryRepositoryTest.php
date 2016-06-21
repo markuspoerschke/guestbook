ecc2bc3a80c979f70b56cd1fb0c9cc0a6e5cd16c
Markus Poerschke <markus.poerschke@trivago.com>
2016-06-21 22:38:45 +0200

    Use $_SERVER instead of $_ENV to make test using DB on travis.

--- tests/Repository/PdoEntryRepositoryTest.php
+++ tests/Repository/PdoEntryRepositoryTest.php
@@ -13,14 +13,14 @@ class PdoEntryRepositoryTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        $db_dsn = isset($_ENV['DB_DSN']) ? $_ENV['DB_DSN'] : false;
+        $db_dsn = isset($_SERVER['DB_DSN']) ? $_SERVER['DB_DSN'] : false;
 
         if (!$db_dsn) {
             $this->markTestSkipped('No DB available.');
         }
 
-        $db_user = isset($_ENV['DB_USER']) ? $_ENV['DB_USER'] : 'root';
-        $db_password = isset($_ENV['DB_PASSWORD']) ? $_ENV['DB_PASSWORD'] : '';
+        $db_user = isset($_SERVER['DB_USER']) ? $_SERVER['DB_USER'] : 'root';
+        $db_password = isset($_SERVER['DB_PASSWORD']) ? $_SERVER['DB_PASSWORD'] : '';
         $this->repository = new PdoEntryRepository(new \PDO($db_dsn, $db_user, $db_password));
     }
 
