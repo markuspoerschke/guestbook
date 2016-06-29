eeaa616dee51d0e916dfea38df3837af2b574044
Markus Poerschke <markus.poerschke@trivago.com>
2016-06-30 01:53:02 +0200

    Added fixtures and application code.

---
+++ etc/fixtures.sql
@@ -0,0 +1,6 @@
+INSERT INTO `guestbook`
+(`author_name`, `author_email`, `body`, `created_at`)
+VALUES
+("Hans", "hans@peter.example.com", "Toller Eintrag Lorem Ipsum...", "2016-05-23 15:54:21"),
+("Max Mustermann", "max.mustermann@example.net", "This is another guestbook entry.", "2016-05-21 12:32:42"),
+("John Doe", "john.doe@example.com", "Dear Author, this is a really nice article!", "2016-05-23 15:54:21");
---
+++ web/index.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$repository = new \Eluceo\Guestbook\Repository\PdoEntryRepository(new \PDO(
+    getenv('GUESTBOOK_DB_DSN'), getenv('GUESTBOOK_DB_USER'), getenv('GUESTBOOK_DB_PASSWORD')
+));
+
+/** @var \Eluceo\Guestbook\Entity\Entry[] $entries */
+$entries = $repository->getAll();
+
+?>
+
+<html>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Author</th>
+                <th>Date</th>
+                <th>Body</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($entries as $entry): ?>
+                <tr>
+                    <td>
+                        <a href="mailto:<?php echo $entry->getAuthorEmail()?>">
+                            <?php echo $entry->getAuthorName() ?>
+                        </a>
+                    </td>
+                    <td>
+                        <?php echo $entry->getCreatedAt()->format('d.m.Y') ?>
+                    </td>
+                    <td>
+                        <?php echo $entry->getBody() ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</body>
+</html>
